91945444d79a0bb279287a207fecfc784522542e
Vladimir Saykovsky <vsk@outsi.de>
2024-12-12 13:07:43 +0300

    fix: find coordinates util

--- src/utils/findVilliagesInfo.ts
+++ src/utils/findVilliagesInfo.ts
@@ -5,14 +5,35 @@ export const findVilliagesInfo = () => {
 
   const villiages = new Map<number, { id: number; x: string; y: string }>();
 
-  document.querySelectorAll("[data-did][data-x][data-y]").forEach((v) => {
-    const id = Number(v.getAttribute("data-did"));
-    const x = v.getAttribute("data-x");
-    const y = v.getAttribute("data-y");
-    if (id && x !== null && y !== null) {
-      villiages.set(id, { id, x, y });
+  const multipleVilliages = document.querySelectorAll("[data-did][data-x][data-y]");
+  if (multipleVilliages.length) {
+    multipleVilliages.forEach((v) => {
+      const id = Number(v.getAttribute("data-did"));
+      const x = v.getAttribute("data-x");
+      const y = v.getAttribute("data-y");
+      if (id && x !== null && y !== null) {
+        villiages.set(id, { id, x, y });
+      }
+    });
+  } else {
+    const textX = document.querySelector(".coordinateX")?.textContent;
+    const textY = document.querySelector(".coordinateY")?.textContent;
+    const id = currentVillageId;
+    if (!textX || !textY) {
+      throw new Error("Not found coordinates!");
     }
-  });
+    const x = getCoordinateFromString(textX);
+    const y = getCoordinateFromString(textY);
+    villiages.set(id, { id, x, y });
+  }
 
   return { currentVillageId, villiages };
 };
+
+function getCoordinateFromString(coord: string) {
+  const num = coord.match(/\d+/g)?.[0];
+  if (num === undefined) {
+    throw new Error("Coordinates not found!");
+  }
+  return coord.length - num.length > 3 ? `-${num}` : num;
+}

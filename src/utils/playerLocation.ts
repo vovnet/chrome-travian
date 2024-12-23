202d217d7eb31a049fc804cba9a5cbf41e31c87e
Vladimir Saykovsky <vsk@outsi.de>
2024-12-23 21:04:58 +0300

    fix: added lang title

--- src/utils/playerLocation.ts
+++ src/utils/playerLocation.ts
@@ -1,19 +1,24 @@
+import langs from "./langs";
+
 const language = document
   .getElementsByClassName("playerProfile")?.[0]
   .textContent?.match(/"language":"([a-z]{2}-[A-Z]{2})"/)?.[1];
 
 const flag = language?.slice(3, 5).toLocaleLowerCase();
 
+const profile = langs.find((v) => v.flag === flag);
+
 const userFlag = document.createElement("img");
 userFlag.className = "languageFlag";
 userFlag.src = `../../../../img/svg/flags/${flag}.svg`;
+userFlag.title = profile?.countryEnglish || "";
 
 const delay = () => {
   setTimeout(() => {
     const img = document.querySelector("img.languageFlag");
     img?.before(userFlag);
 
-    console.log("player location: ", { language, flag, img, userFlag });
+    console.log("player location: ", { language, flag, img, userFlag, profile });
   }, 200);
 };
 
